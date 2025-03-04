5a46a57832c9a8424a29dd17f72ae6694df7b86d
Gourav Rajak <gouravrajak+github@gmail.com>
2025-03-04 17:13:45 +0530

    Backend Update kjgsjkgjk

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+// Routes Import
+import userRoutes from "./routes/user.routes.js"
+
+// Routes Declaration
+app.use("/api/v1/users", userRoutes)
+
+// http://localhost:8000/api/v1/users/register
+
 export { app }
